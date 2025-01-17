366eb59efec3a7dc3bc61be960f17d9061658876
Ken VanDine <ken@vandine.org>
2025-01-17 10:24:19 -0500

    Added ListBox for content

--- src/sysinfo.py
+++ src/sysinfo.py
@@ -1,5 +1,6 @@
 import gi
 gi.require_version('Adw', '1')
+gi.require_version('Gtk', '4.0')
 from gi.repository import Adw, Gtk
 
 class SysInfo(Adw.Bin):
@@ -10,5 +11,42 @@ class SysInfo(Adw.Bin):
         self.set_margin_start(20)
         self.set_margin_end(20)
         
-        label = Gtk.Label(label="System Information")
-        self.set_child(label)
+        # Create a box to hold the header and content
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+
+        # Create a header
+        header = Adw.HeaderBar()
+        header.set_decoration_layout("")  # Remove window controls
+        header.set_title_widget(Gtk.Label(label="System Information"))
+
+        # Create a list box to hold the rows
+        list_box = Gtk.ListBox()
+        list_box.set_selection_mode(Gtk.SelectionMode.NONE)
+
+        # Create Adwaita rows
+        row1 = Adw.EntryRow()
+        row1.set_title("CPU")
+        row1.set_text("Intel Core i7")
+        row1.set_editable(False)
+
+        row2 = Adw.EntryRow()
+        row2.set_title("Memory")
+        row2.set_text("16 GB")
+        row1.set_editable(False)
+
+        row3 = Adw.EntryRow()
+        row3.set_title("Disk")
+        row3.set_text("512 GB SSD")
+        row1.set_editable(False)
+
+        # Add rows to the list box
+        list_box.append(row1)
+        list_box.append(row2)
+        list_box.append(row3)
+
+        # Add header and list box to the vertical box
+        vbox.append(header)
+        vbox.append(list_box)
+
+        # Add the vertical box to the page
+        self.set_child(vbox)
