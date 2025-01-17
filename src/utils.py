a41181909e4ffe85ef2ae72190ec21b51fe44c6c
Drew VanDine <avandine23@gmail.com>
2025-01-17 14:10:43 -0500

    Implemented get_mem_info, get_cpu_info, get_hostname

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,27 @@ class Utils():
         pass
 
     # Return size of the root drive
-    def get_disks(self):
-        return(psutil.disk_usage('/').total/ (1024 ** 3))
+    def get_disk(self):
+        return (str(int(psutil.disk_usage('/').total/ (1024 ** 3))))
+
+    # Return host name
+    def get_hostname(self):
+        f = open('/etc/hostname', 'r')
+        hostname = f.read()
+        f.close()
+
+        return hostname.rstrip()
+
+    # Return MemTotal
+    def get_mem(self):
+        mem_info = {}
+        with open('/proc/meminfo') as f:
+            for line in f:
+                if line.strip():
+                    key, value = line.split(':', 1)
+                    mem_info[key.strip()] = value.strip()
+        mem_size = int(mem_info['MemTotal'].split(" ")[0]) / 1000 ** 2
+        return str(int(mem_size))
 
     # Return CPU model info
     def get_cpu_info(self):
@@ -22,5 +41,5 @@ class Utils():
 
 if __name__ == "__main__":
     utils = Utils()
-    print("Disk Capacity: " + str(utils.get_disks()) + " GB")
+    print("Disk Capacity: " + str(utils.get_disk()) + " GB")
     print("CPU Model: " + utils.get_cpu_info())
