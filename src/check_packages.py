9fe11c7252a3f187d1f6220d3e6aee9688a3d42b
Ken VanDine <ken@vandine.org>
2025-02-05 18:30:50 -0500

    bug: Don't duplicate packages on navigation

--- src/check_packages.py
+++ src/check_packages.py
@@ -13,7 +13,11 @@ class CheckPackages(Adw.Bin):
         self.set_margin_start(20)
         self.set_margin_end(20)
         self.title = "Check Software"
-        self.passed = False
+        # Used to keep references to the Adw.ActionRow for each snap
+        self.known_snap_rows = {}
+        # Used to keep references to the Adw.ActionRow for each deb
+        self.known_deb_rows = {}
+        self.passed = True
 
         # Create vbox
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
@@ -22,41 +26,58 @@ class CheckPackages(Adw.Bin):
         scrolled_window = Gtk.ScrolledWindow()
         scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
 
-        check_snaps_row = Adw.ExpanderRow(title="Check Snaps")
-        snaps_installed = self.utils.check_snaps(snap_packages)
-        for snap in snaps_installed.keys():
-            row = Adw.ActionRow(title=snap)
-            check_snaps_row.add_row(row)
-            if not snaps_installed[snap]:
-                row.set_icon_name("emblem-important-symbolic")
-                button = Gtk.Button(label='Fix')
-                button.connect('clicked', self.on_fix_clicked, snap)
-                row.add_suffix(button)
-                check_snaps_row.set_expanded(True)
-            else:
-                row.set_icon_name("emblem-ok-symbolic")
+        self.check_snaps_row = Adw.ExpanderRow(title="Check Snaps")
+        self.check_debs_row = Adw.ExpanderRow(title="Check System Packages")
 
-        check_debs_row = Adw.ExpanderRow(title="Check System Packages")
-        debs_installed = self.utils.check_debs(deb_packages)
-        for deb in debs_installed.keys():
-            row = Adw.ActionRow(title=deb)
-            check_debs_row.add_row(row)
-            if not debs_installed[deb]:
-                row.set_icon_name("emblem-important-symbolic")
-                button = Gtk.Button(label="Fix")
-                button.connect('clicked', self.on_fix_clicked, deb)
-                row.add_suffix(button)
-                check_debs_row.set_expanded(True)
-            else:
-                row.set_icon_name("emblem-ok-symbolic")
-
-        vbox.append(check_snaps_row)
-        vbox.append(check_debs_row)
+        vbox.append(self.check_snaps_row)
+        vbox.append(self.check_debs_row)
         scrolled_window.set_child(vbox)
         self.set_child(scrolled_window)
 
     def on_fix_clicked(self, button, package):
         print('on_fix_clicked: ' + package)
 
+    # on_shown is called when the page is shown in the stack
     def on_shown(self):
-        pass
+        self.passed = True
+        snaps_installed = self.utils.check_snaps(snap_packages)
+        for snap in snaps_installed.keys():
+            if not snap in self.known_snap_rows.keys():
+                row = Adw.ActionRow(title=snap)
+                # Keep track of ActionRows to prevent duplication
+                self.known_snap_rows[snap] = row
+                self.check_snaps_row.add_row(row)
+            else:
+                row = self.known_snap_rows[snap]
+
+            # If not installed flag
+            if not snaps_installed[snap]:
+                row.set_icon_name("emblem-important-symbolic")
+                button = Gtk.Button(label='Fix')
+                button.connect('clicked', self.on_fix_clicked, snap)
+                row.add_suffix(button)
+                self.check_snaps_row.set_expanded(True)
+                self.passed = False
+            else:
+                row.set_icon_name("emblem-ok-symbolic")
+
+        debs_installed = self.utils.check_debs(deb_packages)
+        for deb in debs_installed.keys():
+            if not deb in self.known_deb_rows.keys():
+                row = Adw.ActionRow(title=deb)
+                # Keep track of ActionRows to prevent duplication
+                self.known_deb_rows[deb] = row
+                self.check_debs_row.add_row(row)
+            else:
+                row = self.known_deb_rows[deb]
+
+            # If not installed flag
+            if not debs_installed[deb]:
+                row.set_icon_name("emblem-important-symbolic")
+                button = Gtk.Button(label="Fix")
+                button.connect('clicked', self.on_fix_clicked, deb)
+                row.add_suffix(button)
+                self.check_debs_row.set_expanded(True)
+                self.passed = False
+            else:
+                row.set_icon_name("emblem-ok-symbolic")
