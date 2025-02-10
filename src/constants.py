27e5fc14caed6d3deca96fa728c76788fcb7ed9d
Ken VanDine <ken@vandine.org>
2025-02-10 16:45:06 -0500

    Added kramden-device to require apt packages

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,2 @@
-deb_packages = ['kramden-overrides', 'kramden-desktop', 'libreoffice-writer']
+deb_packages = ['kramden-overrides', 'kramden-desktop', 'kramden-device', 'libreoffice-writer']
 snap_packages = ['frogsquash', 'gnome-2048', 'gnome-chess', 'gnome-nibbles', 'gnome-weather', 'iagno', 'mc-installer', 'quadrapassel', 'snap-store', 'spotify', 'vlc', 'zoom-client']
