2e1e9195cfc8a2ed4bc999b820239ba8670eb67b
Ken VanDine <ken@vandine.org>
2026-05-13 22:27:22 -0400

    fix: Also use /opt/dell/dcc/cctk to check for BIOS password

--- scripts/bios_password.sh
+++ scripts/bios_password.sh
@@ -2,7 +2,13 @@
 
 echo "Checking for BIOS Password"
 admin_pass=$(lshw -c system|grep configuration|grep administrator_password=enabled)
-if [[ ${#admin_pass} > 0 ]];
+if [ -x "/opt/dell/dcc/cctk" ]; then
+    dell_admin_pass=$(/opt/dell/dcc/cctk --PasswordLock | grep Enabled)
+else
+    dell_admin_pass=""
+fi
+
+if [[ ${#admin_pass} > 0 || ${#dell_admin_pass} > 0 ]];
 then
     echo "BIOS Password enabled"
     exit 1
