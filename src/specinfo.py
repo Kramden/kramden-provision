b8039c0462e155fbf286a3e3d627a80c85fb9c00
Drew VanDine <avandine23@gmail.com>
2026-07-29 16:42:21 -0400

    fix: Build spec_v3 not spec

--- src/specinfo.py
+++ src/specinfo.py
@@ -477,7 +477,7 @@ class SpecInfo(Adw.Bin):
         if self._gathered.get("asset_info") and not self.asset_info_override:
             return False
         if BLOCK_NEXT_WHEN_DRIVE_PRESENT and self.has_disks and not self.disk_override:
-            return False
+            return True
         return True
 
     def get_failure_reasons(self):
